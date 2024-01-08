a97d901400b1b07f4e8238965feab593eb5f6d3a
Temesgen <temekahsay@gmail.com>
2024-01-08 23:57:30 +0300

    springboot kotlin project added

---
+++ springboot-kotlin-rest-api/src/main/kotlin/temesgen/girmay/springbootkotlinrestapi/SpringbootKotlinRestApiApplication.kt
@@ -0,0 +1,11 @@
+package temesgen.girmay.springbootkotlinrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinRestApiApplication>(*args)
+}
---
+++ springboot-kotlin-rest-api/src/test/kotlin/temesgen/girmay/springbootkotlinrestapi/SpringbootKotlinRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package temesgen.girmay.springbootkotlinrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
